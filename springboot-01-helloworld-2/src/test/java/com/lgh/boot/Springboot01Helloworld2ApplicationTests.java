a717f4c2fc871c91c07a6bc7b739b8deae8bbfa4
hhhui-L <1184131956@qq.com>
2021-04-29 10:22:28 +0800

    04.29

---
+++ springboot-01-helloworld-2/src/test/java/com/lgh/boot/Springboot01Helloworld2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgh.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01Helloworld2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
